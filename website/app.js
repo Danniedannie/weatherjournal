6428da75d785505513c7ed673ae85477a7707230
Danniedannie <danielle.simone.lynch@gmail.com>
2019-12-29 16:34:53 +0000

    Add partial post method to server

--- website/app.js
+++ website/app.js
@@ -5,50 +5,55 @@
 let d = new Date();
 let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 
-//api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=d37ead94fdc7e472dfa0bfc189991efb
-
-//console.log(process.env.API_KEY);
-
 // get your element on the page
-
-// const location = ...
-
 
 const button = document.getElementById('generate')
 
+// get the data back as an object
+
+// store the information you want from the response into a new variable
+// send that to the server
+
 button.addEventListener('click', function(event) {
 
+    const API = "d37ead94fdc7e472dfa0bfc189991efb"
     const city = document.getElementById('zip').value;
     const feelings = document.getElementById('feelings').value;
-    const API = "d37ead94fdc7e472dfa0bfc189991efb"
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},can&APPID=${API}`).then(res => res.json())
         .then(posts => {
             const weatherArray = posts;
-            const forecast = {
+            let myFeelings = {
+                feelings: feelings,
                 weather: weatherArray.weather[0],
                 city: weatherArray.name,
                 temp: weatherArray.main
 
             }
+            console.log(myFeelings);
+            const postData = async(url = '', data = {}) => {
+                console.log(data);
+                const response = await fetch(url, {
+                    method: 'POST',
+                    credentials: 'same-origin',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    // Body data type must match "Content-Type" header        
+                    //body: JSON.stringify(data),
+                });
 
+                // try {
+                //     const newData = await response.json();
+                //     console.log(newData);
+                //     return newData;
+                // } catch (error) {
+                //     //console.log("error", error);
+                // }
+            }
 
-            console.log(forecast);
-            console.log(feelings);
-
-
-
+            postData('/addentry', myFeelings)
         })
-});
 
+    // send that to the server
 
-
-// get the data back as an object
-
-// store the information you want from the response into a new variable
-
-// const myFeelings = {
-// ... the weather
-// ... your feelings
-// }
-
-/* send that to the server */
+})
