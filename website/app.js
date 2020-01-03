870a1ecaeda03f0d871cb98da826734c0d1e2706
Danniedannie <danielle.simone.lynch@gmail.com>
2020-01-03 13:46:08 +0000

    finalizing API calls and server side code

--- website/app.js
+++ website/app.js
@@ -2,8 +2,6 @@
 /* Global Variables */
 
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 
 // get your element on the page
 
@@ -16,26 +14,45 @@ let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 
 // send that to the server
 
-let baseURL = 'http://api.openweathermap.org/data/2.5/weather?q='
-let apiKey = 'can&APPID=d37ead94fdc7e472dfa0bfc189991efb';
+const BASEURL = 'http://api.openweathermap.org/data/2.5/weather?q='
+const APIKEY = 'can&APPID=d37ead94fdc7e472dfa0bfc189991efb';
+
 document.getElementById('generate').addEventListener('click', performAction);
 
-function performAction(e) {
+async function performAction() {
+    let d = new Date();
+    let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear() + d.getMilliseconds();
     const feelings = document.getElementById('feelings').value;
     const city = document.getElementById('zip').value + ",";
-    getWeather(baseURL, city, apiKey)
 
-    .then(function(data) {
-        //add data to POST request
-        postData('/addentry', { weather: data.weather, weatherInformation: data.main, cityName: data.name, feelings: feelings });
+    /*     getWeather(BASEURL, city, APIKEY)
+            .then(async function(data) {
+                //add data to POST request
+                const { weather, main, name: cityName } = data;
+                postData('/addentry', {
+                    weather,
+                    weatherInformation: main,
+                    cityName,
+                    feelings,
+                    date: newDate
+                });
+            }); */
+
+    const data = await getWeather(BASEURL, city, APIKEY);
+    const { weather, main, name: cityName } = data;
+    const response = await postData('/addentry', {
+        weather,
+        weatherInformation: main,
+        cityName,
+        feelings,
+        date: newDate
     });
-
+    console.log(response)
 };
 
-const getWeather = async(baseURL, city, key) => {
-
-    const res = await fetch(baseURL + city + apiKey)
+const getWeather = async(BASEURL, city, key) => {
     try {
+        const res = await fetch(BASEURL + city + APIKEY)
 
         const data = await res.json();
         console.log(data)
@@ -48,16 +65,24 @@ const getWeather = async(baseURL, city, key) => {
 
 
 const postData = async(url = '', data = {}) => {
-    const response = await fetch(url, {
-        method: 'POST',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        //body: data type must match "Content-Type" header
-        body: JSON.stringify(data),
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            //body: data type must match "Content-Type" header
+            body: JSON.stringify(data),
+        })
+        return response;
+    } catch (error) {
+        console.log("error", error);
+    }
 
-    });
+
+
+
     //      try {
     //          const newData = data;
     //          console.log(newData);
